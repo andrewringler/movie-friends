a423ccebe616cea1ccce8953deb461b25b27489d
Andrew Ringler <ringler@gmail.com>
2012-10-14 20:38:52 -0400

    pull entire Netflix catalog

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var http = require('http'),
 	fs = require('fs'),
 	util = require('util'),
-	rest = require('restler'),
 	OAuthSimple = require('OAuthSimple'),
 	mikealRequest = require('request');
 
@@ -12,24 +11,11 @@ fs.readFile('./config.json', "utf8", function(error, result) {
 	oauth = new OAuthSimple(config.consumerKey, config.sharedSecret)
     var request = oauth.sign({
       action: "GET",
-      path: "http://api-public.netflix.com/catalog/titles",
-      parameters: { term: 'rock', start_index: '0', max_results: '1', output: 'json' }
+      path: 'http://api.netflix.com/catalog/titles/full',
+      parameters: { v: '2.0' }
     });
 
-	var options = {
-		method: 'GET',
-		headers: { 'accept-encoding': 'gzip,deflate' }
-	};
 	console.log(request.signed_url);
-
 	mikealRequest(request.signed_url).pipe(fs.createWriteStream('./data/netflix-full-catalog'))
-	
-	// rest.request(request.signed_url, options).on('complete', function(result) {
-	// 	if(result instanceof Error) {
-	// 		console.log('Error\n' + result);
-	// 	} else {
-	// 		console.log('Success\n' + result);
-	// 	}
-	// });
 });
 
